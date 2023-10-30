907ff66d8f601025e63c616f4d33e7c3235adeaa
SubashKrishnan <starboy3712@gmail.com>
2023-10-30 14:40:58 +0530

    go router added for navigation along with import formatter

---
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -0,0 +1,34 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:go_router/go_router.dart';
+
+// Project imports:
+import 'package:scr_vendor/core/app_route_constants.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppPage.home.title),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            context.goNamed(AppPage.login.name);
+          },
+          child: const Text("Login"),
+        ),
+      ),
+    );
+  }
+}
