8667d44666845787eee6e015474d53c8a348cead
SubashKrishnan <starboy3712@gmail.com>
2023-11-14 15:25:50 +0530

    feat: Integrate Sentry for Error Tracking
    Integrated Sentry for enhanced error tracking and logging.Ensured Sentry initialization in the main function for capturing all app-level exceptions.

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -6,4 +6,7 @@ class AppConfig {
       'ap-south-1:b134905c-2e40-455c-b1d4-802a56941f9c';
   static const String apiEndpoint =
       'https://1ubsx75vi5.execute-api.ap-south-1.amazonaws.com/dev';
+
+  static const String sentryDsn =
+      'https://7ed5b7fa1394de2435e800276b1c4104@o4506218470637568.ingest.sentry.io/4506218478567424';
 }
