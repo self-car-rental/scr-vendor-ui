500e39aa060e00e5177cc0454578e9513b5a386c
SubashKrishnan <starboy3712@gmail.com>
2023-11-21 12:43:49 +0530

    chore: Update Cognito info and API base URL in app config

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,11 +1,11 @@
 class AppConfig {
   static const String region = 'ap-south-1';
-  static const String userPoolId = 'ap-south-1_94fkvbwAg';
-  static const String appClientId = '5795jto817ajla9na16ab37kae';
+  static const String userPoolId = 'ap-south-1_ggVpUv6Ds';
+  static const String appClientId = '32f7cadp8g1c12ba6ck33ig7hg';
   static const String cognitoIdentityPoolId =
-      'ap-south-1:b134905c-2e40-455c-b1d4-802a56941f9c';
+      'ap-south-1:922bfb16-c01c-4fb4-bf03-0c5d4dda0c72';
   static const String apiEndpoint =
-      'https://1ubsx75vi5.execute-api.ap-south-1.amazonaws.com/dev';
+      'https://3p81o4iopb.execute-api.ap-south-1.amazonaws.com/dev';
 
   static const String sentryDsn =
       'https://7ed5b7fa1394de2435e800276b1c4104@o4506218470637568.ingest.sentry.io/4506218478567424';
